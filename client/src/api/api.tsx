c24615bebe84010bfc78b1749d7ad7b6a6bf5e97
YannV <yann.e.verdier@gmail.com>
2024-10-14 11:04:38 +0200

    add commentary

--- client/src/api/api.tsx
+++ client/src/api/api.tsx
@@ -4,6 +4,15 @@ export const endpoints = {
     helloworld: "api/",
 }
 
+
+/**
+ * Function that centralizes fetch requests.
+    The request method is detected automatically
+    She works with the above endpoints.
+ * @param endpoint property of above endpoints
+ * @param datas object for method POST body
+ * @returns result of request (will probably change to update the context)
+ */
 export  function requestAPI(endpoint: string, datas?: Object){
     let url = `http://localhost:3006/${endpoint}`;
     let options: RequestInit = {
