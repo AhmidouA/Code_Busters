932c3a6c75a9fa0834a8e7452b007ab83b6bf3c6
YannV <yann.e.verdier@gmail.com>
2024-10-15 00:11:53 +0200

    add context provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,21 @@
 import LoginCard from './components/loginCard';
 import { Route, Routes } from 'react-router-dom';
+import { UserContext, DatasContext } from './datas/context';
 import './style.css';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState({});
+  const [user, setUser] = useState(null);
+
   return (
-    <Routes>
-        <Route path="/" element={<LoginCard />} />
-    </Routes>
+    <UserContext.Provider value={user}>
+      <DatasContext.Provider value={data}>
+        <Routes>
+            <Route path="/" element={<LoginCard />} />
+        </Routes>
+      </DatasContext.Provider>
+    </UserContext.Provider>
   );
 }
 
